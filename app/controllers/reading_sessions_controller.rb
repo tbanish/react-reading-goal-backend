9a928b57d748bf3b82cba92cf01ccc63cf885aff
tbanish <tombanish@gmail.com>
2021-04-16 14:14:01 -0400

    build index action for reading sessions

--- app/controllers/reading_sessions_controller.rb
+++ app/controllers/reading_sessions_controller.rb
@@ -1,2 +1,6 @@
 class ReadingSessionsController < ApplicationController
+  def index
+    reading_sessions = Book.find_by(id: params[:book_id]).reading_sessions
+    render json: ReadingSessionsSerializer.new(reading_sessions)
+  end
 end
