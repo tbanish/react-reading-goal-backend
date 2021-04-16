2d5d3986b8df08fe084795661cb1a51a4893104a
tbanish <tombanish@gmail.com>
2021-04-16 14:13:25 -0400

    nest reading_sessions routes under books and limit routes to index, create, update and destroy

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :reading_sessions
-  resources :books
+  resources :books, only: [:index, :create, :update, :destroy] do
+    resources :reading_sessions, only: [:index, :create, :update, :destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
