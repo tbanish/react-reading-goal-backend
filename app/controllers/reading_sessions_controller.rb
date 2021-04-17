8694e2c04864f258f01579720bd34c2b456a1c44
tbanish <tombanish@gmail.com>
2021-04-17 16:31:37 -0400

    build create action and reading_session_params in ReadingSessionsController

--- app/controllers/reading_sessions_controller.rb
+++ app/controllers/reading_sessions_controller.rb
@@ -3,4 +3,20 @@ class ReadingSessionsController < ApplicationController
     reading_sessions = Book.find_by(id: params[:book_id]).reading_sessions
     render json: ReadingSessionsSerializer.new(reading_sessions)
   end
+
+  def create
+    reading_session = ReadingSession.new(reading_session_params)
+    
+    if reading_session.save
+      render json: ReadingSessionsSerializer.new(reading_session)
+    else
+      render json: {errors: reading_session.erros.full_messages}
+    end
+  end
+
+  private
+
+  def reading_session_params
+    params.require(:reading_session).permit(:title, :duration, :date, :book_id)
+  end
 end
