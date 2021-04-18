3b780a8e561d8a92469756241e3eaf1ad199de2d
tbanish <tombanish@gmail.com>
2021-04-18 18:02:04 -0400

    nest note routes under books

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :notes
+
   resources :books, only: [:index, :create, :update, :destroy] do
     resources :reading_sessions, only: [:index, :create, :update, :destroy]
+    resources :notes, only: [:index, :create, :update, :destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
