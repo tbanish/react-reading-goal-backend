1f74d49547508c230447a24baf311bbeb6611d05
tbanish <tombanish@gmail.com>
2021-04-18 18:07:27 -0400

    add seed data for notes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+# NOTE_CONTENT
+notes = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+
 # BOOKS (NOT FINISHED)
 pastoral = Book.create(title: "American Pastoral", author: "Philip Roth")
 day = Book.create(title: "Against the Day", author: "Thomas Pynchon")
@@ -14,4 +17,12 @@ dayOne = ReadingSession.create(title: "Pages 1-30", duration: "1 hour", date: "F
 mountainOne = ReadingSession.create(title: "Chapters 1-2", duration: "1 hour 20 minutes", date: "Friday, April 16, 2021", book_id: mountain.id)
 meditationsOne = ReadingSession.create(title: "Chapter 1-5", duration: "2 hours 10 minutes", date: "Friday, April 16, 2021", book_id: meditations.id)
 swanOne = ReadingSession.create(title: "Pages 1-10", duration: "5 minutes", date: "Friday, April 16, 2021", book_id: swann.id)
-bengOne = ReadingSession.create(title: "Pages 1-45", duration: "50 mintes", date: "Friday, April 16, 2021", book_id: being.id)
+beingOne = ReadingSession.create(title: "Pages 1-45", duration: "50 mintes", date: "Friday, April 16, 2021", book_id: being.id)
+
+# NOTES
+pastoralOneNoteOne = Note.create(title: "pastoralOneNoteOne", content: notes, reading_session_id: pastoralOne.id)
+dayOneNoteOne = Note.create(title: "dayOneNoteOne", content: notes, reading_session_id: dayOne.id)
+mountainOneNoteOne = Note.create(title: "mountainOneNoteOne", content: notes, reading_session_id: mountainOne.id)
+meditationsOneNoteOne = Note.create(title: "meditationsOneNoteOne", content: notes, reading_session_id: meditationsOne.id)
+swanOneNoteOne = Note.create(title: "swanOneNoteOne", content: notes, reading_session_id: swanOne.id)
+bengOneNoteOne = Note.create(title: "bengOneNoteOne", content: notes, reading_session_id: beingOne.id)
