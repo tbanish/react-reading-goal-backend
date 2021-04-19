92e42c358bc2fa5d6bfcbb8d7feeebf0b14c157f
tbanish <tombanish@gmail.com>
2021-04-19 19:55:43 -0400

    limit goal routes to index, create, update, destroy

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     resources :reading_sessions, only: [:index, :create, :update, :destroy]
     resources :notes, only: [:index, :create, :update, :destroy]
   end
+  resources :goals, only: [:index, :create, :update, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
