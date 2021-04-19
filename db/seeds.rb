d572ec8c7d670de51c037de1680ac22a3b85ac20
tbanish <tombanish@gmail.com>
2021-04-19 13:35:47 -0400

    reseed test data with duration set to integer

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,12 @@ swann = Book.create(title: "Swann's Way", author: "Marcel Proust", finished: tru
 being = Book.create(title: "The Unbearable Lightness of Being", author: "Milan Kundera", finished: true)
 
 # READING SESSIONS
-pastoralOne = ReadingSession.create(title: "Chapter 1", duration: "30 minutes", date: "Friday, April 16, 2021", book_id: pastoral.id)
-dayOne = ReadingSession.create(title: "Pages 1-30", duration: "1 hour", date: "Friday, April 16, 2021", book_id: day.id)
-mountainOne = ReadingSession.create(title: "Chapters 1-2", duration: "1 hour 20 minutes", date: "Friday, April 16, 2021", book_id: mountain.id)
-meditationsOne = ReadingSession.create(title: "Chapter 1-5", duration: "2 hours 10 minutes", date: "Friday, April 16, 2021", book_id: meditations.id)
-swanOne = ReadingSession.create(title: "Pages 1-10", duration: "5 minutes", date: "Friday, April 16, 2021", book_id: swann.id)
-beingOne = ReadingSession.create(title: "Pages 1-45", duration: "50 mintes", date: "Friday, April 16, 2021", book_id: being.id)
+pastoralOne = ReadingSession.create(title: "Chapter 1", duration: 30, date: "Friday, April 16, 2021", book_id: pastoral.id)
+dayOne = ReadingSession.create(title: "Pages 1-30", duration: 60, date: "Friday, April 16, 2021", book_id: day.id)
+mountainOne = ReadingSession.create(title: "Chapters 1-2", duration: 80, date: "Friday, April 16, 2021", book_id: mountain.id)
+meditationsOne = ReadingSession.create(title: "Chapter 1-5", duration: 130, date: "Friday, April 16, 2021", book_id: meditations.id)
+swanOne = ReadingSession.create(title: "Pages 1-10", duration: 5, date: "Friday, April 16, 2021", book_id: swann.id)
+beingOne = ReadingSession.create(title: "Pages 1-45", duration: 50, date: "Friday, April 16, 2021", book_id: being.id)
 
 # NOTES
 pastoralOneNoteOne = Note.create(title: "pastoralOneNoteOne", content: notes, reading_session_id: pastoralOne.id)
