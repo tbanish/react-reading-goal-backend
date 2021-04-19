c091ad3cc0b8775b3abfec854fdb12925c853a18
tbanish <tombanish@gmail.com>
2021-04-18 21:11:40 -0400

    update strong params to accept note attributes and update create action to create new notes with reading session

--- app/controllers/reading_sessions_controller.rb
+++ app/controllers/reading_sessions_controller.rb
@@ -5,9 +5,11 @@ class ReadingSessionsController < ApplicationController
   end
 
   def create
-    reading_session = ReadingSession.new(reading_session_params)
+    reading_session = ReadingSession.new(title: reading_session_params[:title], date: reading_session_params[:date], duration: reading_session_params[:duration], book_id: reading_session_params[:book_id])
+    notes = reading_session_params[:notes_attributes]
 
     if reading_session.save
+      notes.each{|note| Note.create(title: note[:title], content: note[:content], reading_session_id: reading_session.id)}
       render json: ReadingSessionsSerializer.new(reading_session)
     else
       render json: {errors: reading_session.erros.full_messages}
@@ -33,6 +35,6 @@ class ReadingSessionsController < ApplicationController
   private
 
   def reading_session_params
-    params.require(:reading_session).permit(:title, :duration, :date, :book_id)
+    params.require(:reading_session).permit(:title, :duration, :date, :book_id, notes_attributes: [:id, :title, :content])
   end
 end
