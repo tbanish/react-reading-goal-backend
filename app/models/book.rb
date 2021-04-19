64d48c1dcf035c386104e9280106728602826d51
tbanish <tombanish@gmail.com>
2021-04-19 18:57:55 -0400

    add scope method :finished, returns all books that are finished

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
   has_many :reading_sessions, dependent: :destroy
   has_many :notes, through: :reading_sessions
+  scope :finished, -> { where(finished: true) }
+
 end
