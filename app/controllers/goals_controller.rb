9d2e7b425a3eec1a7e180ec659cd6ae7b4e79256
tbanish <tombanish@gmail.com>
2021-04-20 09:45:26 -0400

    build create action and strong params in goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -3,4 +3,20 @@ class GoalsController < ApplicationController
     goals = Goal.all
     render json: GoalsSerializer.new(goals)
   end
+
+  def create
+    goal = Goal.new(goal_params)
+    goal.remaining = goal.total - Book.finished.count
+    if goal.save
+      render json: GoalsSerializer.new(goal)
+    else
+      render json: {errors: goal.errors.full_messages}
+    end
+  end
+
+  private
+
+  def goal_params
+    params.require(:goal).permit(:total, :remaining)
+  end
 end
