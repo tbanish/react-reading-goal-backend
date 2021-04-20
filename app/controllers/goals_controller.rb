3a5bd8fc5481239348dc73825a4da99a2f2b47b6
tbanish <tombanish@gmail.com>
2021-04-20 10:18:16 -0400

    build update action in goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -14,6 +14,17 @@ class GoalsController < ApplicationController
     end
   end
 
+  def update
+    goal = Goal.find_by(id: params[:id])
+    goal.update(goal_params)
+    goal.remaining = goal.total - Book.finished.count
+    if goal.save
+      render json: GoalsSerializer.new(goal)
+    else
+      render json: {errors: goal.errors.full_messages}
+    end
+  end
+
   private
 
   def goal_params
