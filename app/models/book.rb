5d635015ebb99fc5ce815ab653cd54dcf310befd
tbanish <tombanish@gmail.com>
2021-04-27 18:57:05 -0400

    validates :title, :author, presence: true

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   has_many :reading_sessions, dependent: :destroy
   has_many :notes, through: :reading_sessions
+  validates :title, :author, presence: true
   scope :finished, -> { where(finished: true) }
 
 end
